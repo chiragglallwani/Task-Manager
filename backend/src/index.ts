f36840b5919a8c4eecfcc636508685ffed4e0086
Chirag Lalwani <lalwanichiragsocial3@gmail.com>
2025-11-26 15:34:02 +0530

    CORS fixing error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,8 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
