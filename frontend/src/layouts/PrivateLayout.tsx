494b7ccdf72958e76b057e71719ea1c3063398f7
Chirag Lalwani <lalwanichiragsocial3@gmail.com>
2025-11-26 22:11:11 +0530

    Update Loader UI

--- frontend/src/layouts/PrivateLayout.tsx
+++ frontend/src/layouts/PrivateLayout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Navigation/Navbar";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { useAuth } from "@/hooks/useAuth";
+import { Loader2Icon } from "lucide-react";
 import { Navigate } from "react-router-dom";
 
 export const PrivateLayout = ({ children }: { children: React.ReactNode }) => {
@@ -8,8 +9,9 @@ export const PrivateLayout = ({ children }: { children: React.ReactNode }) => {
 
   if (isLoading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="text-center text-foreground">Loading...</div>
+      <div className="min-h-screen flex items-center justify-center w-full">
+        <Loader2Icon className="w-10 h-10 animate-spin text-primary" />
+        <div className="text-centertext-lg text-primary">Loading...</div>
       </div>
     );
   }
